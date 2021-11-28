c1d1363cfdbd001be15f2d8141fd47f030f9165e
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-11-28 13:41:47 +0800

    [bili] 03. 状态栏处理

--- app/src/main/java/com/jesen/compose_bili/MainActivity.kt
+++ app/src/main/java/com/jesen/compose_bili/MainActivity.kt
@@ -7,7 +7,10 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.jesen.compose_bili.ui.theme.Compose_bili_talkTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,9 +18,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Compose_bili_talkTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+                // 加入ProvideWindowInsets
+                ProvideWindowInsets {
+                    // 状态栏改为透明
+                    rememberSystemUiController().setStatusBarColor(
+                        Color.Transparent, darkIcons = MaterialTheme.colors.isLight
+                    )
+                    // 底部导航栏
+                    rememberSystemUiController().setNavigationBarColor(
+                        Color.Transparent, darkIcons = MaterialTheme.colors.isLight
+                    )
+                    // A surface container using the 'background' color from the theme
+                    Surface(color = MaterialTheme.colors.background) {
 
+                    }
                 }
             }
         }
