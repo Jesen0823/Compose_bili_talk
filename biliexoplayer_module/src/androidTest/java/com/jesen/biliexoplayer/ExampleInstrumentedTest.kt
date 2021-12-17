0163845cd9365a22cc29143972136957d7e0d81c
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-12-17 11:10:15 +0800

    [bili-banner] 29. banner组件 修改名称

--- biliexoplayer_module/src/androidTest/java/com/jesen/biliexoplayer/ExampleInstrumentedTest.kt
+++ biliexoplayer_module/src/androidTest/java/com/jesen/biliexoplayer/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.jesen.biliexoplayer", appContext.packageName)
+        assertEquals("com.jesen.biliexoplayer_module", appContext.packageName)
     }
 }
